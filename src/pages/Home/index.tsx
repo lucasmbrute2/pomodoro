ca018ebab38f49da1fb53ca39dfedb0304bed7b3
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-01-15 18:25:15 -0300

    feat: home strcture

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,39 @@
+import { Play } from "phosphor-react";
+import { CountDownContainer, FormContainer, HomeContainer } from "./styles";
+
 export function Home() {
-    return <h1>Ola</h1>;
+    return (
+        <HomeContainer>
+            <form>
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <input
+                        id="task"
+                        type="text"
+                        placeholder="Dê um nome para o seu projeto"
+                    />
+                    <label htmlFor="minutesAmount">durante</label>
+
+                    <span>-</span>
+                    <input id="minutesAmount" type="text" />
+                    <span>+</span>
+
+                    <p>minutos.</p>
+                </FormContainer>
+
+                <CountDownContainer>
+                    <span>0</span>
+                    <span>0</span>
+                    <span>:</span>
+                    <span>0</span>
+                    <span>0</span>
+                </CountDownContainer>
+
+                <button type="submit">
+                    <Play size={24} />
+                    Começar
+                </button>
+            </form>
+        </HomeContainer>
+    );
 }
